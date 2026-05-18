e06d84b0f441cd78f1584bb367a503ae8ee78f7d
meryemammari23 <meryemammari08@gmail.com>
2026-05-18 03:20:10 +0100

    vesion final

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
